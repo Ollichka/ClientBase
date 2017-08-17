d16ebcd7501f311a6b26066b234e246206d7d6f9
Ollichka <salnikova.olia@gmail.com>
2017-08-18 01:11:39 +0300

    add and delete files

---
+++ Backend/data/Client_List.js
@@ -0,0 +1,14 @@
+
+var client_info = [
+    {
+        id:1,
+        name: "Salnikova Olia",
+        phone: '0937712481',
+        email: 'salnikova.olia@gmail.com',
+        status: 1,
+        additional_info:"no info"
+
+    }
+];
+
+module.exports = client_info;
---
+++ Backend/data/Product_List.js
@@ -0,0 +1,15 @@
+
+var product_info = [
+    {
+        id:1,
+        name: "multy 350",
+        category: 'home',
+        articl: '011930',
+        price_client: 100,
+        price_own:75,
+        ball:10,
+        additional_info:'no info'
+    }
+];
+
+module.exports = product_info;
